f04c53f8ed620020df5239bfd5503fe2f0842d35
Edu <luis_100404@hotmail.com>
2024-12-30 21:56:54 -0300

    update certificados.jsx

--- src/pages/Certificados.jsx
+++ src/pages/Certificados.jsx
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar";
 import Style from "./Certificados.module.css";
 
+////icons
+
+
 const Certificados = () => {
   return (
     <div className={Style.divPrincCertificados}>
@@ -8,8 +11,44 @@ const Certificados = () => {
         <Navbar></Navbar>
         <h1 className={Style.titleCertificado}>Certificados</h1>
 
-        <div>
+        <div className={Style.divPrincDivs}>
           
+          <div>
+            <h1>Curso de JavaScript do básico ao avançado - Luiz Otávio Miranda</h1>
+          </div>
+          
+          <div>
+            <h1>Formação Node.js - Guia do Programador, Victor Lima</h1>
+          </div>
+
+          <div>
+            <h1>React do Zero a Maestria - Matheus Battisti, Hora de Codar</h1>
+          </div>
+
+          <div>
+            <h1>Minicurso de Java - Rocketseat</h1>
+          </div>
+
+          <div>
+            <h1>Discover - Rocketseat</h1>
+          </div>
+
+          <div>
+            <h1>NLW Unite - Reactjs - Rocketseat (Evento)</h1>
+          </div>
+
+          <div>
+            <h1>Imersão front-end - Alura</h1>
+          </div>
+
+          <div>
+            <h1>NLW Unite - Python - Rocketseat (Evento)</h1>
+          </div>
+
+          <div>
+            <h1>Imersão Python: Do Excel à Análise de Dados - Alura</h1>
+          </div>
+
         </div>
 
     </div>
